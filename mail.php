cb63d1dc7e3c13f6b3fae71ce655547fd4dd8c01
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2022-04-16 16:58:03 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@ $message= $_POST['message'];
 $to = "dhanarajappu130@gmail.com";
 $subject = "Mail From portfolio website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@https://dhanarajappu456.github.io/website/" . "\r\n" .
+$headers = "From: noreply@dhanarajappu456.github.io/website/" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
