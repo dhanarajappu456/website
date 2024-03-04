772fceecc83ef616b833d3df95e679d1d8567f0b
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-03-04 16:16:00 +0530

    Update script.js
    Dynamic Leetcode Count Update

--- script.js
+++ script.js
@@ -59,37 +59,70 @@ $(document).ready(function () {
     loop: true,
   });
 });
+
+const f = async () => {
+  try {
+    const resp = await fetch(
+      "https://leetcode-api-faisalshohag.vercel.app/dan_stark123"
+    );
+    const d = await resp.json();
+    const probsSolved = [d["easySolved"], d["mediumSolved"], d["hardSolved"]];
+    const total = [d["totalEasy"], d["totalMedium"], d["totalHard"]];
+    const totalSolved = d["totalSolved"];
+
+    console.log;
+    return [probsSolved, total, totalSolved];
+  } catch (err) {
+    console.log("error");
+  }
+};
+
+console.log("meme");
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+const problemFunction = async () => {
+  const problemInfo = await f();
+  const totalSolved = problemInfo[2];
+  document.querySelector(".prob-count").textContent = totalSolved;
+  document.querySelector(".easy").textContent = problemInfo[0][0];
+  document.querySelector(".medium").textContent = problemInfo[0][1];
+  document.querySelector(".hard").textContent = problemInfo[0][2];
+  let circularProgressArr = document.querySelectorAll(".circular-progress"),
+    progressValueArr = document.querySelectorAll(".progress-value");
+  fractionProb = [
+    (problemInfo[0][0] * 100) / problemInfo[1][0],
+    (problemInfo[0][1] * 100) / problemInfo[1][1],
+    (problemInfo[0][2] * 100) / problemInfo[1][2],
+  ];
 
-let circularProgressArr = document.querySelectorAll(".circular-progress"),
-  progressValueArr = document.querySelectorAll(".progress-value");
-fractionProb = [22800 / 778, 42600 / 1612, 11400 / 682];
+  for (let i = 0; i < 3; i++) {
+    let circularProgress = circularProgressArr[i];
+    let progressValue = progressValueArr[i];
 
-for (let i = 0; i < 3; i++) {
-  let circularProgress = circularProgressArr[i];
-  let progressValue = progressValueArr[i];
+    let progressStartValue = 0;
+    let progressEndValue = fractionProb[i];
+    console.log(progressEndValue);
+    speed = 190;
 
-  let progressStartValue = 0;
-  let progressEndValue = fractionProb[i];
-  console.log(progressEndValue);
-  speed = 190;
+    let progress = setInterval(() => {
+      progressStartValue++;
 
-  let progress = setInterval(() => {
-    progressStartValue++;
+      progressValue.textContent = `${progressStartValue}%`;
+      circularProgress.style.background = `conic-gradient(#7d2ae8 ${
+        progressStartValue * 3.6
+      }deg, #ededed 0deg)`;
 
-    progressValue.textContent = `${progressStartValue}%`;
-    circularProgress.style.background = `conic-gradient(#7d2ae8 ${
-      progressStartValue * 3.6
-    }deg, #ededed 0deg)`;
+      if (progressStartValue >= progressEndValue) {
+        clearInterval(progress);
+      }
+    }, speed);
+  }
+};
 
-    if (progressStartValue >= progressEndValue) {
-      clearInterval(progress);
-    }
-  }, speed);
-}
+problemFunction();
+
 // let progressStartValue = 0,
 //   progressEndValue = 76800 / 3072,
 //   speed = 100;
